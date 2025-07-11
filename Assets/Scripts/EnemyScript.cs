796179e056f0d31a6a4c0a7398c916f70e42a11d
Programmering <teknisk+programmering-GDC2025-GR2@game.unf.dk>
2025-07-11 14:28:47 +0200

    Del 1 (opgave) færdig

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hello I am evil");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
